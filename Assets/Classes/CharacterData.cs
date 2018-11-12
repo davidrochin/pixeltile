74ae0a826e4f258b0c6f4c4b9aeee2bf03bb2e76
David Rochín <jdrc8@hotmail.com>
2018-11-12 14:56:39 -0700

    Se empezó a crear el sistema de animación

--- Assets/Classes/CharacterData.cs
+++ Assets/Classes/CharacterData.cs
@@ -11,7 +11,44 @@ public class CharacterData : ScriptableObject {
     [Range(0.1f, 10f)]
     public float speed = 1f;
 
-    [Header("Graphics")]
-    public Sprite spriteSheet;
+    public CharacterGraphics graphics;
 
+}
+
+[System.Serializable]
+public class CharacterGraphics {
+    public Sprite spriteSheet;
+    public float shadowSize = 0.5f;
+
+    public Sprite[] GetSprites(AnimationType animationType) {
+        List<Sprite> sprites = new List<Sprite>();
+        switch (animationType) {
+            case AnimationType.IdleSouth:
+                sprites.Add(GetSprite(0));
+                break;
+        }
+        return sprites.ToArray();
+    }
+
+    public Sprite[] GetAnimation(int row) {
+        List<Sprite> sprites = new List<Sprite>();
+        int posX = 0; int posY = ((int)spriteSheet.rect.height - TILE_SIZE_Y) - TILE_SIZE_Y * row;
+        int maxSprites = (int)spriteSheet.rect.width / TILE_SIZE_X;
+        for (int x = 0; x < maxSprites; x++) {
+            sprites.Add(Sprite.Create(spriteSheet.texture, new Rect(x * TILE_SIZE_X, posY, TILE_SIZE_X, TILE_SIZE_Y), new Vector2(0.5f, 0.5f), PPU));
+        }
+        return sprites.ToArray();
+    }
+
+    public Sprite GetSprite(int number) {
+        int posX = 0; int posY = (int)spriteSheet.rect.height - TILE_SIZE_Y;
+        return Sprite.Create(spriteSheet.texture, new Rect(posX, posY, TILE_SIZE_X, TILE_SIZE_Y), new Vector2(0.5f, 0.5f), PPU);
+    }
+
+    public const int SHEET_COUNT_X = 4;
+    public const int SHEET_COUNT_Y = 6;
+    public const int TILE_SIZE_X = 32;
+    public const int TILE_SIZE_Y = 34;
+    public const float PPU = 22.62742f;
+    public enum AnimationType { IdleSouth, IdleWest, IdleEast, IdleNorth }
 }
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,13 +7,34 @@ public class Character : MonoBehaviour {
     public CharacterData data;
 
     SpriteRenderer renderer;
+    CharacterMotor motor;
+
+    Coroutine animationCoroutine;
 
     private void Awake() {
         renderer = transform.GetComponentInChildren<SpriteRenderer>();
     }
 
     private void Start() {
-        Sprite sprite = Sprite.Create(data.spriteSheet.texture, new Rect(0, 0, 32, 34), new Vector2(0.5f, 0.5f), 22.62742f);
-        renderer.sprite = sprite;
+        PlayAnimation(data.graphics.GetAnimation(0), 0.5f);
+    }
+
+    public void PlayAnimation(Sprite[] animation, float frequency) {
+        if(animationCoroutine != null) {
+            StopCoroutine(animationCoroutine);
+        }
+        animationCoroutine = StartCoroutine(AnimationCoroutine(animation, frequency));
+    }
+
+    private IEnumerator AnimationCoroutine(Sprite[] animation, float frequency) {
+        int index = 0;
+        while (true) {
+            if(index >= animation.Length) {
+                index = 0;
+            }
+            renderer.sprite = animation[index];
+            index++;
+            yield return new WaitForSeconds(frequency);
+        }
     }
 }
