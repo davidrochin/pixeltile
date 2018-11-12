8e15213553f59c685e6ab4ceb3d7809dc8823b8a
David Rochin <jdrc8@hotmail.com>
2018-11-12 09:10:20 -0700

    Cambios a los sprites

---
+++ Assets/Classes/CharacterData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Name", menuName = "Character Data", order = 1)]
+public class CharacterData : ScriptableObject {
+
+    [Header("Information")]
+    public string name;
+
+    [Range(0.1f, 10f)]
+    public float speed = 1f;
+
+    [Header("Graphics")]
+    public Sprite spriteSheet;
+
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public CharacterData data;
+
+    SpriteRenderer renderer;
+
+    private void Awake() {
+        renderer = transform.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start() {
+        Sprite sprite = Sprite.Create(data.spriteSheet.texture, new Rect(0, 0, 32, 34), new Vector2(0.5f, 0.5f), 22.62742f);
+        renderer.sprite = sprite;
+    }
+}
