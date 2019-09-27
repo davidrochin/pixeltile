7c4bd23c6e54a479d862a127badb46f562126076
David Rochín <jdrc8@hotmail.com>
2019-09-27 15:55:14 -0600

    Implemented new sprites

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -33,9 +33,9 @@ public class Character : MonoBehaviour {
 
     private void Start() {
         currentDirection = DetermineDirection();
-        PlayAnimation(data.graphics.GetAnimation(AnimationType.Running, currentDirection), 0.1f);
+        //PlayAnimation(data.graphics.GetAnimation(AnimationType.Running, currentDirection), 0.1f);
 
-        motor.OnFrameFinish += delegate () {
+        /*motor.OnFrameFinish += delegate () {
 
             CardinalDirection cardinalDirection = DetermineDirection();
             if (cardinalDirection != currentDirection) {
@@ -45,28 +45,48 @@ public class Character : MonoBehaviour {
                 currentDirection = cardinalDirection;
             }
             
-        };
+        };*/
 
-        motor.OnGroundedChange += delegate () {
+        /*motor.OnGroundedChange += delegate () {
             if (motor.Grounded) {
                 PlayAnimation(data.graphics.GetAnimation(motor.Moving ? AnimationType.Running : AnimationType.Idle, currentDirection), 0.1f);
             } else {
                 PlayAnimation(data.graphics.GetAnimation(AnimationType.Falling, currentDirection), 0.1f);
             }
-        };
+        };*/
 
-        motor.OnWalk += delegate () {
+        /*motor.OnWalk += delegate () {
             CardinalDirection cardinalDirection = DetermineDirection();
             PlayAnimation(data.graphics.GetAnimation(AnimationType.Running, cardinalDirection), 0.1f);
-        };
+        };*/
 
-        motor.OnStop += delegate () {
+        /*motor.OnStop += delegate () {
             CardinalDirection cardinalDirection = DetermineDirection();
             PlayAnimation(data.graphics.GetAnimation(AnimationType.Idle, cardinalDirection), 0.1f);
-        };
+        };*/
     }
 
     private void Update() {
+
+        // Manage Input
+        Vector3 v = Vector3.zero;
+
+        if (GetInputMagnitude() > 0.05f) {
+            motor.TurnTowards(GetInputVector());
+            motor.Walk(GetInputVector() * GetInputMagnitude() * 1f);
+            v = GetInputVector() * GetInputMagnitude() * 1f;
+        }
+
+        motor.animator.SetFloat("Move/North", Vector3.Dot(v, new Vector3(1f, 0f, 1f).normalized));
+        motor.animator.SetFloat("Move/East", Vector3.Dot(v, new Vector3(1f, 0f, -1f).normalized));
+
+        if (Input.GetButtonDown("Jump")) {
+            motor.Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            Debug.Break();
+        }
 
         // Manage shadow
         RaycastHit hit;
@@ -76,7 +96,57 @@ public class Character : MonoBehaviour {
         } else {
             shadowRenderer.enabled = false;
         }
+
+        
     }
+
+    public InputType inputType;
+
+
+    Vector3 GetInputVector() {
+        Vector3 input = Vector3.zero;
+
+        if (inputType == InputType.Normal) {
+            input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        } else if (inputType == InputType.Raw) {
+            input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        //if (virtualJoystick != null) { input = new Vector3(input.x + virtualJoystick.input.x, 0f, input.y + virtualJoystick.input.y); }
+
+        //Transformar la direccion para que sea relativa a la camara.
+        //Vector3 transDirection = Camera.main.transform.TransformDirection(input);
+        Quaternion tempQ = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
+        //Debug.Log(tempQ);
+        Vector3 transDirection = tempQ * input;
+
+        //Hacer que el Vector no apunte hacia arriba.
+        //transDirection = new Vector3(transDirection.x, 0f, transDirection.z).normalized;
+        finalMovementVector = transDirection;
+        return transDirection.normalized;
+    }
+
+    float GetInputMagnitude() {
+        Vector3 input = Vector3.zero;
+
+        // Get Input from standard Input methods
+        if (inputType == InputType.Normal) { input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")); } else if (inputType == InputType.Raw) { input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); }
+
+        // Get Input from Virtual Joystick if available
+        //if (virtualJoystick != null) { input = new Vector3(input.x + virtualJoystick.input.x, 0f, input.y + virtualJoystick.input.y); }
+
+        // Clamp magnitude to 1
+        return Vector3.ClampMagnitude(input, 1f).magnitude;
+    }
+
+    Vector3 finalMovementVector;
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, finalMovementVector);
+    }
+
+    public enum InputType { Normal, Raw }
 
     public void PlayAnimation(Sprite[] animation, float frequency) {
         if(animationCoroutine != null) {
